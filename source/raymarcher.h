ad2254661451b9074a1f7817687a4ca45a72fc78
Rico <RicoP@users.noreply.github.com>
2023-01-09 01:06:41 +0100

    use sokol-shdc.h

--- source/raymarcher.h
+++ source/raymarcher.h
@@ -1,61 +1,7 @@
 
 #include "raylib.h"
-
-
-
-//Pseudo SOKOL header to include shader stuff
-#define SOKOL_GFX_INCLUDED
-enum ShaderUniformType { SG_UNIFORMTYPE_NONE = 0, SG_UNIFORMTYPE_FLOAT4 };
-enum ShaderImageType { SG_IMAGETYPE_NONE = 0, SG_IMAGETYPE_2D };
-enum ShaderSamplerType { SG_SAMPLERTYPE_NONE = 0, SG_SAMPLERTYPE_FLOAT };
-enum sg_backend { SG_BACKEND_NONE = 0, SG_BACKEND_GLCORE33, SG_BACKEND_GLES2 };
-enum sg_layout { SG_UNIFORMLAYOUT_NONE = 0, SG_UNIFORMLAYOUT_STD140 };
-
-struct ShaderAttribute {
-  const char *name = "";
-};
-
-struct ShaderUniform {
-  const char *name = "";
-  ShaderUniformType type = SG_UNIFORMTYPE_NONE;
-  int array_count = 0;
-  int location = -1;  // Set after loading.
-};
-
-struct ShaderImages {
-  const char *name = "";
-  ShaderImageType type = SG_IMAGETYPE_NONE;
-  ShaderSamplerType sampler_type = SG_SAMPLERTYPE_NONE;
-};
-
-struct ShaderUniformBlock {
-  int size = 0;
-  int layout = 0;
-  ShaderUniform uniforms[8];
-};
-
-struct ShaderSource {
-  const char *source = "";
-  const char *entry = "";
-  ShaderUniformBlock uniform_blocks[8];
-  ShaderImages images[8];
-};
-
-struct ShaderDescription {
-  ShaderAttribute attrs[8];
-  ShaderSource vs;
-  ShaderSource fs;
-  const char *label = "";
-};
-
-typedef ShaderDescription sg_shader_desc;
-
+#include <rose/sokol-shdc.h>
 #include "shader.h"
-
-
-
-
-#define GLSL_VERSION            330
 
 Shader shader;
 
